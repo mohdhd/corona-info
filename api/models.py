c5728a35a8086742c3c4a27851abf13796388303
mohamed <mohamedhishamben@gmail.com>
2020-03-25 15:41:15 +0200

    edit model

--- api/models.py
+++ api/models.py
@@ -14,7 +14,13 @@ class State(models.Model):
     recovered = models.IntegerField(default=0)
     confirmed = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.name
+
 class News(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(max_length=2000)
     date = models.DateField()
+
+    def __str__(self):
+        return self.title
